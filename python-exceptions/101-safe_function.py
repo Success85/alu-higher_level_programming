c59eef84d15f1be7db1669d5c9b0b5abfa50ff00
Success-1 <itumasuccess29@gmail.com>
2025-10-29 15:36:46 +0200

    Added 101-safe_function.py

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    """Executes a function safely and handles exceptions."""
+    try:
+        return fct(*args)
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        return None
