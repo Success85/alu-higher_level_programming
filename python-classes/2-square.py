2fd8dd107c1f1d124a87a3504acd68e29ae85634
Success-1 <itumasuccess29@gmail.com>
2025-11-08 17:06:07 +0200

    Added 2-square.py file

---
+++ python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """This is the class Square and defining its size. And the size must be reater than or equal to 0"""
+
+    def __init__(self, size=0):
+        self.__size = size
+~
+~
