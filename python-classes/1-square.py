bf0244b2cd08f16bfeacacc0f1d33f51e64d0e85
Success-1 <itumasuccess29@gmail.com>
2025-11-08 17:01:25 +0200

    Added 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """This is the class Square and defining it size."""
+
+    def __init__(self, size):
+        self.__size = size
