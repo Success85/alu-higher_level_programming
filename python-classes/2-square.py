4a20a51cda8b1b3b4809ec61cb7a0a75226caa5e
Success-1 <itumasuccess29@gmail.com>
2025-11-08 17:09:17 +0200

    Added 2-square.py file

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,7 +5,11 @@
 class Square:
     """This is the class Square and defining its size. And the size must be reater than or equal to 0"""
 
-    def __init__(self, size=0):
+    if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
 ~
 ~
