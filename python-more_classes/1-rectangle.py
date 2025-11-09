4b6b525c0173c739d4e87c47b73022f27e7538e4
Success-1 <itumasuccess29@gmail.com>
2025-11-09 13:11:52 +0200

    Added 1-rectangle file

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,12 +3,32 @@
 
 
 class Rectangle:
-    """This represent the class rectangle."""
+    """This represent a rectangle."""
 
-    def __init__(self, size):
-        """Initialize a Rectangle.
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
-        Args:
-            size (int): The size of the new rectangle.
-        """
-        self.__size = size
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
