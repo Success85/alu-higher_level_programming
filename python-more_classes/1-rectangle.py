ee556da3686d12d80d4069b2ec85eb13547e223f
Success-1 <itumasuccess29@gmail.com>
2025-11-09 13:05:08 +0200

    Added 1-rectangle file

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Rectangle."""
+
+
+class Rectangle:
+    """This represent the class rectangle."""
+
+    def __init__(self, size):
+        """Initialize a Rectangle.
+
+        Args:
+            size (int): The size of the new rectangle.
+        """
+        self.__size = size
