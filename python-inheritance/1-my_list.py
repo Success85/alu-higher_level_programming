f11f1acfc7a5047e67378358526d74942c3a4515
Success-1 <itumasuccess29@gmail.com>
2025-11-13 02:41:56 +0200

    Fixed some errors

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that defines a MyList class that inherits from list"""
 
+
 class MyList(list):
 
     """Custom list class with a print_sorted method"""
--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module to check if an object is exactly an instance of a specified class"""
 
+
 def is_same_class(obj, a_class):
 
     """Returns True if obj is exactly an instance of a_class"""
