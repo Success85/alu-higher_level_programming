047aaac45cd7f77cce429fdbafe86e58441a7441
Success-1 <itumasuccess29@gmail.com>
2025-11-14 16:07:12 +0200

    Added 7-add_item.py file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""Adds all command-line arguments to a JSON list in 'add_item.json'."""
-
+"""
+Script that adds all arguments to a Python list, and then save them to a file.
+"""
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-"""Load existing items from the JSON file, or start with an empty list."""
 try:
     items = load_from_json_file(filename)
 except Exception:
     items = []
 
-"""Add command-line arguments (excluding the script name)."""
 items.extend(sys.argv[1:])
-
-"""Save the updated list back to the JSON file."""
 save_to_json_file(items, filename)
